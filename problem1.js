a4e091cfae75323a02071fe18a741b9e24170add
Dianjeruto <145572102+Dianjeruto@users.noreply.github.com>
2023-10-09 06:03:21 +0000

    completed assignments

---
+++ problem1.js
@@ -0,0 +1,21 @@
+function calculateGrade(){
+    //User enters student marks here
+    let marks = prompt("Enter Marks between 0 and 100: ");
+
+    // if statement to generate grade basement on grading system
+    if (marks > 79) {
+        console.log("The grade is A")
+    }else if (marks >= 60 && marks <= 79){
+        console.log("The grade is B")
+    }else if (marks > 49 && marks <= 59){
+        console.log("The grade is C")
+    }else if (marks >= 40 && marks <= 49){
+        console.log("The grade is D")
+    }else if(marks <40){
+        console.log("The grade is E")
+    }else{ //Error to be generated if marks are out of range
+        console.log("Error! Marks not within grading range")
+    }
+}
+
+calculateGrade()
---
+++ problem2.js
@@ -0,0 +1,14 @@
+function speedLimit(speed){
+    // user to enter vehicle speed
+    let speed = prompt("Enter Speed of the Vehicle: ");
+    // speed below or equal to 70 is okay
+    if (speed <= 70) {
+        console.log("Okay")
+    }else if (speed > 70){ //user to earn one demerit point for each 5 units of speed surpassed
+        console.log( `${(speed-70)/5} Demerit Points Earned` )
+    }else{
+        console.log("Error!")
+    }
+}
+
+speedLimit()
